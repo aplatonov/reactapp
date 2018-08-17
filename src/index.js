8c14d606dc2c53de962155175a1beafee540c109
Anthony Platonov <a.platonov@php73.ru>
2018-08-17 17:07:55 +0400

    Second task. v1.02 add styles, inc/dec buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style.css';
 
 class Timer extends React.Component {
 	constructor(props) {
@@ -11,17 +11,19 @@ class Timer extends React.Component {
 		this.state = {
 			milliseconds: 0,
 		};
+
+		this.interval = 98;
 	}
 
 	componentDidMount() {
 		this.timer = setInterval(() => {
 			this.tick();
-		}, 100);
+		}, this.interval);
 	}
 
 	tick() {
 		this.setState({
-			milliseconds: this.state.milliseconds + 1,
+			milliseconds: this.state.milliseconds + this.interval,
 		})
 	}
 
@@ -29,7 +31,7 @@ class Timer extends React.Component {
 		if (!this.timer) { 
 			this.timer = setInterval(() => {
 				this.tick();
-			}, 100);
+			}, this.interval);
 		}
 	};
 
@@ -38,22 +40,51 @@ class Timer extends React.Component {
 		this.timer = null;
 	};
 
+	onInc = () => {
+		this.setState({
+			milliseconds: this.state.milliseconds + this.interval,
+		})
+	};
+
+	onDec = () => {
+		this.setState({
+			milliseconds: this.state.milliseconds - this.interval,
+		})
+	};
+
 	componentWillUnmount() {
 		clearInterval(this.timer);
 	}
 
+	msToTime = (s) => {
+		let ms = s % 1000;
+		s = (s - ms) / 1000;
+		let secs = s % 60;
+		s = (s - secs) / 60;
+		let mins = s % 60;
+		let hrs = (s - mins) / 60;
+
+		let pad = (n, z = 2) => ('00' + n).slice(-z);
+
+		return hrs + ':' + pad(mins) + ':' + pad(secs) + '.' + pad(ms, 3);
+	}
+
 	render() {
 		return (
-			<div>
-				<h1>{this.state.milliseconds}</h1>
-				<button onClick={this.onClickStart}>Start</button>
-				<button onClick={this.onClickStop}>Stop</button>
+			<div className="timer">
+				<div>Таймер</div>
+				<h1 className="counter">{this.msToTime(this.state.milliseconds)}</h1>
+				<button className="buttonTrue" onClick={this.onClickStart}>Start</button>
+				<button className="buttonFalse" onClick={this.onClickStop}>Stop</button>
+				<br />
+				<button className="buttonTrue" onClick={this.onInc}>Inc</button>
+				<button className="buttonFalse" onClick={this.onDec}>Dec</button>
 			</div>
 		)
 	}
 }
 
 const rootElement = document.getElementById("app");
-ReactDOM.render(<Timer />, rootElement);
+ReactDOM.render(<Timer interval="1000"/>, rootElement);
 
 module.hot.accept();
